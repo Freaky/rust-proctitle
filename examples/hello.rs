e3ccdfdc4ecf9074b43cf6a31b5eec5f45b1c4f0
Thomas Hurst <tom@hur.st>
2019-04-25 03:10:23 +0100

    Make example a bit more interesting

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,7 +1,10 @@
 use proctitle;
 
 fn main() {
-    proctitle::set_title("Hello, world");
-    println!("Title set, go look while I gobble down CPU");
-    loop {}
+    let mut i = 0;
+    loop {
+        proctitle::set_title(format!("Hello, world, {}", i));
+        i += 1;
+        std::thread::sleep(std::time::Duration::from_millis(500));
+    }
 }
