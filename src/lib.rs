fe6ac4c26f696e91a0ff103ee68ea3873fe5963a
Thomas Hurst <tom@hur.st>
2019-04-26 03:40:40 +0100

    First cut of some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,67 @@
+#![deny(missing_docs)]
+
+//! # Cross-platform process titles.
+//!
+//! `proctitle` attempts to expose the closest safe approximation of the BSD
+//! `setproctitle()` function on the platforms it supports.
+//!
+//! This can be useful if you wish to expose some internal state to `top` or `ps`,
+//! or to help an administrator distinguish between multiple instances of your
+//! program.
+//!
+//! ```rust
+//! # fn perform_task(_x: &str) { }
+//! # fn main() {
+//! use proctitle::set_title;
+//! let tasks = ["frobrinate", "defroogle", "hodor", "bork"];
+//!
+//! for task in &tasks {
+//!    set_title(format!("example: {}", task));
+//!    perform_task(task);
+//! }
+//! set_title("example: idle");
+//! # }
+//! ```
+//!
+//! On Linux or a BSD you could then watch `top` or `ps` and see the process name
+//! change as it works:
+//!
+//! ```sh
+//! -% cmd &
+//! [1] 8515
+//! -% ps $!
+//!  PID TT  STAT     TIME COMMAND
+//! 8515  4  S+    0:00.06 example: defroggle (cmd)
+//! ```
+//!
+//! ## Supported Platforms
+//!
+//! ### BSD
+//!
+//! On BSDs, `setproctitle()` is used, and should pretty much Just Work.  Use
+//! `top -a` to see titles.
+//!
+//! ### Linux
+//!
+//! `proctitle` uses `prctl(PR_SET_NAME)` to name the current thread, with a
+//! truncation limit of 15 bytes.
+//!
+//! More BSD-ish process-global changes are possible by modifying the process
+//! environment, but this is not yet supported because it's wildly unsafe.
+//!
+//! ### Windows
+//!
+//! `SetConsoleTitleW()` is used to set a title for the console, if any.
+//!
+//! In case there is no console (for example, a system service), a dummy named
+//! event handle is also created.  This can be found via tools such as Process
+//! Explorer (View ⮕ Lower Pane View ⮕ Handles) and Process Hacker
+//! (Properties ⮕ Handles).
+//!
+//! ### Everything Else
+//!
+//! Unsupported platforms merely receive a stub function that does nothing.
+
 #[cfg(any(
     target_os = "freebsd",
     target_os = "hardenedbsd",
@@ -10,6 +74,7 @@ mod imp {
     use std::ffi::OsStr;
     use std::os::unix::ffi::OsStrExt;
 
+    /// Set a process title, or some approximation of it, if possible.
     pub fn set_title<T: AsRef<OsStr>>(title: T) {
         if let Ok(title) = CString::new(title.as_ref().to_owned().as_bytes()) {
             unsafe {
@@ -31,6 +96,7 @@ mod imp {
     use std::ffi::OsStr;
     use std::os::unix::ffi::OsStrExt;
 
+    /// Set a process title, or some approximation of it, if possible.
     pub fn set_title<T: AsRef<OsStr>>(title: T) {
         if let Ok(title) = CString::new(title.as_ref().to_owned().as_bytes()) {
             unsafe { libc::prctl(libc::PR_SET_NAME, title.as_ptr(), 0, 0, 0) };
@@ -63,13 +129,14 @@ mod imp {
         static ref EVENT_HANDLE: Mutex<Option<Handle>> = Mutex::new(None);
     }
 
-    // Windows doesn't appear to have a userspace mechanism to name the current
-    // process.
-    //
-    // Try to set a console title, and in case we're not attached to one,
-    // follow PostgreSQL's lead and create a named event handle that can be
-    // found in Process Explorer, Process Hacker, etc.
+    /// Set a process title, or some approximation of it, if possible.
     pub fn set_title<T: AsRef<OsStr>>(title: T) {
+        // Windows doesn't appear to have a userspace mechanism to name the current
+        // process.
+        //
+        // Try to set a console title, and in case we're not attached to one,
+        // follow PostgreSQL's lead and create a named event handle that can be
+        // found in Process Explorer, Process Hacker, etc.
         let mut t: Vec<u16> = title.as_ref().encode_wide().take(1024).collect();
         t.push(0);
 
@@ -95,6 +162,7 @@ mod imp {
 mod imp {
     use std::ffi::OsStr;
 
+    /// Set a process title, or some approximation of it, if possible.
     pub fn set_title<T: AsRef<OsStr>>(_title: T) {}
 }
 
